d0fda62dcc0a1f94ff2350aeb83f23711fe6e46b
ProfThiagoFersi <thiagoferreira@prof.educacao.sp.gov.br>
2024-11-11 17:37:10 -0300

    Finalizado

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Programação',
+    'Qual a função da tg div',
+    'A tag <div> no HTML é usada para criar divisões'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior país do mundo em área?',
+    'A Rússia é o maior país do mundo em área'
+)
+
+criaCartao(
+    'Programação',
+    'Qual é a função principal da tag <a> no HTML?',
+    'A tag <a> é usada para criar links de hipertexto'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz azul em Inglês?',
+    'Oi em ingles é Blue'
+)
